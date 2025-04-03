6eae4bb04773fa10813fc6f1dee8fcde0c4b5aa6
RAJNEESH KUMAR <02sameergangwar2003@gmail.com>
2025-04-03 15:09:44 +0530

    Video 55 Completed

---
+++ Video 55/script.js
@@ -0,0 +1,57 @@
+console.log("Hey this is tutorial 55")
+
+// var a =5;
+// // a = a + 1;   //allowed
+// var b =6;
+// var c = "Rajneesh";
+
+// var _x = "Shubham";
+// var 55z = "Rohan";   //not allowed
+
+// console.log(a + b)
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c)
+
+// const a1 = 6;
+// a1 = a1 + 1;    //not allowed because a1 is constant
+
+// var a=5;
+let a=5;
+// a=a+1;
+let b=6;
+let c="Harry"
+let _x="Shubham"
+
+{
+    let a=66;
+    console.log(a)
+}
+console.log(a)
+
+let x = "Harry Bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let O = {
+    name : "Harry",
+    "job code" : 5500,
+    is_handsome : true,
+    "phone number" : "0123456789"
+}
+
+// console.log(typeof O);
+console.log(O);
+O.salary = "100crores";
+console.log(O);
+O.salary = "500crores";
+console.log(O);
+// O.name = "Abhishek";
+// O["job code"] = 8600
+// O["phone number"] = "9791662464"
+// console.log(O);
