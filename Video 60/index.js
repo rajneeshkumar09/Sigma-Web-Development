c05ab84971818cbadd1e28948e8ea06c3f6d60ac
RAJNEESH KUMAR <02sameergangwar2003@gmail.com>
2025-04-15 21:55:02 +0530

    Video 60 Completed

---
+++ Video 60/index.js
@@ -0,0 +1,34 @@
+console.log("This is String tutorial")
+
+let a = "Abhishek";
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+
+console.log(a.length)
+
+let real_name = "Rajneesh";
+let friend = "Rohit";
+
+console.log("His name is " + real_name + " and his friend name is " + friend);
+console.log(`His name is ${real_name} and his friend name is ${friend}`);
+
+let b = "ShiVamSh";
+
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1, 5));
+console.log(b.replace("Sh", "77"));
+
+let c = "Bhai";
+console.log(c.replace("Bhai", "Bhau"));
+
+console.log(a.concat(` ${c}`));
