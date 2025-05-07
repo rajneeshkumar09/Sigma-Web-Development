d1679c4f6e249a60495dc72c577d47c849eba80e
RAJNEESH KUMAR <02sameergangwar2003@gmail.com>
2025-05-08 04:18:00 +0530

    Video 81 Completed

---
+++ Video 81/script.js
@@ -0,0 +1,45 @@
+const addItem = async (item) => {
+    await randomdelay()
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div);
+}
+
+const randomdelay = () => {
+    return new Promise((resolve, reject) => {
+        timeout = 1 + 6 * Math.random();
+        setTimeout(() => {
+            resolve()
+        }, timeout * 1000);
+    });
+}
+
+async function main() { 
+    let t = setInterval(() => {
+        let last = document.body.getElementsByTagName("div");
+        last = last[last.length - 1];
+        if (last.innerHTML.endsWith("...")){
+            last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length-3);
+        }
+        else {
+            last.innerHTML = last.innerHTML + ".";
+        }
+    }, 100);
+
+    let text = [
+        "Initialized Hacking now reading your data",
+        "Reading your Files",
+        "Password files Detected",
+        "Sending all passwords and personal files to server",
+        "Cleaning up"
+    ]
+    
+    for (const item of text) {
+        await addItem(item)
+    }
+
+    await randomdelay();
+    clearInterval(t)
+}
+
+main()
